7e6de302c2fd3a24747ade38e372ba630c384a19
Brent Dillingham <brentdillingham@gmail.com>
2016-06-19 12:51:16 -0400

    Include colorize in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
+require "pry"
+
 require_relative "lib/matchers"
+require_relative "lib/colorize"
 require_relative "lib/test_client"
 require_relative "lib/test_server"
 
-RSpec.configure do |c|
-  c.include Helpers
+RSpec.configure do |config|
+  config.include Helpers
 end
